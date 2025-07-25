08d1e6a1a2153b89ccc55ada542738f3d0c7accb
pooja-1521 <m.pooja1521@gmail.com>
2025-07-25 13:23:35 +0530

    changed

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import speech_recognition as sr
 from polly_speak import speak
 from commands import handle_command
 
-def listen():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("🎤 Listening...")
-        audio = r.listen(source)
-
-        try:
-            command = r.recognize_google(audio)
-            print("🗣️ You said:", command)
-            return command.lower()
-        except sr.UnknownValueError:
-            speak("Sorry, I didn't catch that.")
-            return None
 
 if __name__ == "__main__":
     speak("Hello, I'm your cloud assistant. How can I help you?")
